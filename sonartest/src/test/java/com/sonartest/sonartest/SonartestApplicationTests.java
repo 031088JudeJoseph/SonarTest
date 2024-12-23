5e0c5ab4f94727ac88ee3eb189edf0f624012d16
AzureAD\JudeSanthoshKumarJos <jude.joseph@numeracle.com>
2024-12-23 14:02:29 +0530

    first commit

---
+++ sonartest/src/test/java/com/sonartest/sonartest/SonartestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonartest.sonartest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonartestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
